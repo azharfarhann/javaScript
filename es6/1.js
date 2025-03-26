9862d11160689dd22cca709b3378e0a8fc8a2f66
azharfarhann <azharfarhan785@gmail.com>
2025-03-26 19:53:15 +0000

    es6

---
+++ es6/1.js
@@ -0,0 +1,64 @@
+// Revison of Loops 
+
+// There are 7 types of loops in JS
+// 1. for loop
+// 2. while loop
+// 3. do while loop
+// 4. for Each loop
+// 5. map loop
+// 6. for of loop
+// 7. for in loop
+
+
+// let arr = ["Azhar", "Farhan", "Syed"];
+
+// 1. for loop
+// for loop
+
+//  for(let i=0; i<arr.length; i++){
+//  console.log(arr[i]);
+//  }   // it will print the value of the array
+
+// 2. while loop
+
+// let j = 0;
+// while(j<arr.length){
+//     console.log(arr[j]);
+//     j++;
+// }    // it will print the value of the array
+
+// 3. do while loop
+
+// let k = 0;
+// do{
+//     console.log(arr[k]);
+//     k++;
+// }while(k<arr.length);    // it will print the value of the array
+
+// 4. for Each loop
+
+// arr.forEach((x) =>{
+//     console.log(x);
+// }); // it will print the value of the array
+
+// 5. map loop
+
+// arr.map((x) =>{
+//     console.log(x);
+// }); // it will print the value of the array
+
+// 6. for of loop
+
+// for(let x of arr){
+//     console.log(x);
+// }   // it will print the value of the array
+
+// 7. for in loop
+
+//  for(let x in arr){
+//      console.log(x);
+// }  // it will print the index of the array
+
+
+
+
---
+++ stringMethods/2.js
@@ -0,0 +1,5 @@
+// let num =10;
+// console.log(num.toString(2)); // 1010
+// console.log(num.toString(8)); // 12
+// console.log(num.toString(16)); // a
+// Decimal to Binary All numbers system
