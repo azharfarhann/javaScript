767a49e5069899ae60553c5b93cc9db81a5429d5
azharfarhann <azharfarhan785@gmail.com>
2025-03-27 14:36:55 +0000

    practised 14 Js qstns

---
+++ revisionJs/1.js
@@ -0,0 +1,71 @@
+//1. print hello world
+// console.log("Hello World");
+
+//2. Add two numbers
+let a =10;
+let b =20;
+let c = a +b;
+// console.log(c);
+
+//3. Find the Square root
+let d = 16;
+let e = Math
+// console.log(Math.sqrt(d));
+
+///4. find the areaof triangle
+let base = 16;
+let height = 20;
+let areaValue = 0.5 * base * height;
+// console.log(`the area of triangle is ${areaValue}`);
+
+//5. swap two variables
+let f = 10;
+let g =20;
+let temp = f;
+f = g;
+g = temp;
+// console.log(f, g);//
+
+//6. convert km to miles
+let km = 20;
+let miles = km * 0.6;
+// console.log(miles); 
+
+//7. convert celcius to fahrenheit
+let celcius = 20;
+let fahrenheit = celcius * 1.8 + 32;
+console.log(fahrenheit);
+
+//8. work with const
+const pi = 3.14;
+// console.log(pi);
+
+//9. work with let
+let h = 10;
+// console.log(h);
+
+//10. work with var
+var i = 20;
+// console.log(i);
+
+//11. work with string
+let str = "Azhar";
+// console.log(str);
+
+//12. work with array
+let arr = [10, 20, 30, 40, 50];
+// console.log(arr);
+
+//13. write to console
+// console.log("Hello World");
+
+//14. check if a number is even or odd
+let j =10;
+console.log(j % 2 === 0);
+
+//15. check if a number is positive or negative
+let k =10;
+console.log(k > 0); // true
+console.log(k < 0); // false
+
+
